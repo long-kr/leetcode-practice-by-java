4962718b0386353847755649ea6c0c320f486789
long-kr <longkr.work@gmail.com>
2026-05-11 21:04:45 -0500

    add Range SUm BTS problem

---
+++ src/solution/RangSumBTS.java
@@ -0,0 +1,42 @@
+package solution;
+
+import java.math.BigInteger;
+
+public class RangSumBTS {
+    int sum = 0;
+
+    public int rangeSumBST(TreeNode root, int low, int high) {
+        if (root == null)
+            return 0;
+
+        // BST has left side smaller
+        if (root.val < low)
+            return rangeSumBST(root.right, low, high);
+
+        // BST has right side smaller
+        if (root.val > high)
+            return rangeSumBST(root.left, low, high);
+
+        return root.val + rangeSumBST(root.left, low, high) + rangeSumBST(root.right, low, high);
+    }
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode() {
+        }
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+        TreeNode(int val, TreeNode left, TreeNode right) {
+            this.val = val;
+            this.left = left;
+            this.right = right;
+        }
+    }
+
+}
